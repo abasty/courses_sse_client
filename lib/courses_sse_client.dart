9bd433d5f4d29e1eccd434bdd141b25de32a3730
Alain Basty <alain.basty@free.fr>
2021-05-06 08:04:02 +0200

    Organized imports

--- lib/courses_sse_client.dart
+++ lib/courses_sse_client.dart
@@ -1,9 +1,10 @@
 import 'dart:math';
 
+import 'package:stream_channel/stream_channel.dart';
+
 import 'src/courses_sse_client_stub.dart'
     if (dart.library.io) 'src/courses_sse_client_io.dart'
     if (dart.library.html) 'src/courses_sse_client_html.dart';
-import 'package:stream_channel/stream_channel.dart';
 
 String randomSseClientId() {
   var r = Random().nextInt(1 << 31) + 1;
